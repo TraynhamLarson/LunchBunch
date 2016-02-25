76777cdb9a885c76dd066aa599cd422005ab16b5
Traynham Larson <Traynham14@gmail.com>
2016-02-25 08:40:02 -0500

    backend stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  resources :bites
   devise_for :users
+
+  get 'show' => 'bites/show'
+
+  get 'new' => 'bites/new'
+
+  get 'edit' => 'bites/edit'
+
   root 'bites#index'
 end
