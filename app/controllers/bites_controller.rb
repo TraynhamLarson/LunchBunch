f66619beeda13a0bc38cbf1d20aa8c848bd26f96
April Marie <april.marie.kelly13@gmail.com>
2016-02-26 11:48:12 -0500

    first push of my version of lunchbunch app

--- app/controllers/bites_controller.rb
+++ app/controllers/bites_controller.rb
@@ -1,3 +1,28 @@
 class BitesController < ApplicationController
-  resources :bites
+
+  before_action :set_bite, only: [:show, :edit, :update, :destroy]
+  before_filter :authenticate_user!
+
+  def create
+    @bite = Bite.new(bite_params)
+
+    respond_to do |format|
+      if @bite.save
+        format.html {redirect_to @bite, notice: 'Bite was successfully created.' }
+      else
+        format.html { render :new }
+      end
+  end
+
+  def new
+    @bite = Bite.new
+  end
+
+  def index
+    @b = Bite.all
+  end
+
+  def show
+  end
+  end
 end
