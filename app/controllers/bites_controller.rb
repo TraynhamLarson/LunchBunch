63421c29d0508665a37c2359c2613a6cc85c82f4
Amber Codes <agalvanosi@gmail.com>
2016-02-27 16:55:19 -0500

    app bites controller update

--- app/controllers/bites_controller.rb
+++ app/controllers/bites_controller.rb
@@ -1,3 +1,17 @@
 class BitesController < ApplicationController
   resources :bites
+  before_action :set_bite, only [:show, :edit, :update, :destroy]
+
+  before_filter :authenticate_user!
+
+def create
+  @bite = Bite.new(bite_params)
+
+  respond_to do |format|
+    if @bite.save
+      format.html {redirect_to @bite, notice: 'Bite was sucessfully created.' }
+    else
+    format.html {render :new }
+    end
+  end
 end
